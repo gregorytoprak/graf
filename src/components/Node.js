06710ef46ea6be60c3116b717270a285a99b931a
gdtoprak <gdtoprak@users.noreply.github.com>
2017-02-10 15:23:32 -0500

    Refactors, including using the property initializer es7 syntax

--- src/components/Node.js
+++ src/components/Node.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react'
 
 class Node extends Component {
-  constructor (props) {
-    super(props)
+  state = {}
 
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick (e) {
+  handleClick = (e) => {
     if (e.shiftKey) {
       this.props.deleteNode(this.props.id)
     }
   }
 
+  // handleDrag = (e) => {
+  //   console.log(e)
+  // }
+
   render () {
     return (
-      <circle id={this.props.id} fill='black' cx={this.props.cx} cy={this.props.cy} r='1'
+      <circle id={this.props.id} cx={this.props.x} cy={this.props.y}
         onClick={this.handleClick}
+        // onDrag={this.handleDrag}
+        fill='black' r='1'
       />
     )
   }
--- src/components/Sheet.js
+++ src/components/Sheet.js
@@ -2,59 +2,72 @@ import React, { Component } from 'react'
 import Node from './Node'
 
 class Sheet extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      realDims: { w: 100, h: 100 },
-      rawSheetCoords: {},
-      nodes: []
-    }
-
-    this.createNode = this.createNode.bind(this)
-    this.deleteNode = this.deleteNode.bind(this)
+  state = {
+    dims: { w: 100, h: 100 },
+    rawDims: { x: 0, y: 0, w: 1, h: 1 },
+    nodes: []
   }
 
   componentDidMount () {
     const r = document.getElementById('Sheet').getBoundingClientRect()
-    const rawSheetCoords = {
+    const rawDims = {
       x: r.left,
       y: r.top,
       w: r.right - r.left,
       h: r.bottom - r.top
     }
-    this.setState({ rawSheetCoords })
+    this.setState({ rawDims })
   }
 
-  createNode (e) {
+  rawToReal = (rawLoc) => {
+    return {
+      x: (rawLoc.x - this.state.rawDims.x) * (this.state.dims.w / this.state.rawDims.w),
+      y: (rawLoc.y - this.state.rawDims.y) * (this.state.dims.h / this.state.rawDims.h)
+    }
+  }
+
+  createNode = (e) => {
     if (e.shiftKey) { return }
-    const sheet = this.state.rawSheetCoords
+    const loc = this.rawToReal({ x: e.pageX, y: e.pageY })
     const node = {
       id: Date.now(),
-      cx: (e.pageX - sheet.x) * this.state.realDims.w / sheet.w,
-      cy: (e.pageY - sheet.y) * this.state.realDims.h / sheet.h
+      x: loc.x,
+      y: loc.y
     }
     this.setState({ nodes: [...this.state.nodes, node] })
   }
 
-  deleteNode (nodeId) {
+  deleteNode = (nodeId) => {
     this.setState({
       nodes: this.state.nodes.filter(node => node.id !== nodeId)
     })
   }
 
+  // moveNode = (nodeId, rawNewLoc) => {
+  //   const newLoc = this.rawToReal(rawNewLoc)
+  //   this.setState({
+  //     nodes: this.state.nodes.map(node => {
+  //       if (node.id === nodeId) {
+  //         return {
+  //           id: node.id,
+  //           x: newLoc.x,
+  //           y: newLoc.y
+  //         }
+  //       } else {
+  //         return node
+  //       }
+  //     })
+  //   })
+  // }
+
   render () {
-    const viewBox = `0 0 ${this.state.realDims.w} ${this.state.realDims.h}`
+    const viewBox = `0 0 ${this.state.dims.w} ${this.state.dims.h}`
     return (
       <svg id='Sheet' className='Sheet' style={{ background: '#bbb' }} viewBox={viewBox}
         onClick={this.createNode}
       >
         {this.state.nodes.map((node) =>
-          <Node
-            key={node.id}
-            id={node.id}
-            cx={node.cx}
-            cy={node.cy}
+          <Node key={node.id} {...node}
             deleteNode={this.deleteNode}
           />
         )}
