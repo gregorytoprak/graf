cdb9ec7d1ff4708fbf234f5705e1ad9015c193a6
gdtoprak <gdtoprak@users.noreply.github.com>
2017-02-09 21:04:33 -0500

    Spaces around braces for object notation

--- src/components/Sheet.js
+++ src/components/Sheet.js
@@ -5,7 +5,7 @@ class Sheet extends Component {
     super(props)
 
     this.state = {
-      realDims: {w: 100, h: 100},
+      realDims: { w: 100, h: 100 },
       rawSheetCoords: {},
       nodes: []
     }
@@ -16,12 +16,13 @@ class Sheet extends Component {
 
   componentDidMount () {
     const r = document.getElementById('Sheet').getBoundingClientRect()
-    this.setState({rawSheetCoords: {
+    const rawSheetCoords = {
       x: r.left,
       y: r.top,
       w: r.right - r.left,
       h: r.bottom - r.top
-    }})
+    }
+    this.setState({ rawSheetCoords })
   }
 
   handleClick (event) {
@@ -33,7 +34,7 @@ class Sheet extends Component {
       cx: (click.x - sheet.x) * this.state.realDims.w / sheet.w,
       cy: (click.y - sheet.y) * this.state.realDims.h / sheet.h
     }
-    this.setState({nodes: [...this.state.nodes, node]})
+    this.setState({ nodes: [...this.state.nodes, node] })
   }
 
   renderNode (node) {
@@ -45,7 +46,7 @@ class Sheet extends Component {
   render () {
     const viewBox = `0 0 ${this.state.realDims.w} ${this.state.realDims.h}`
     return (
-      <svg id='Sheet' className='Sheet' style={{background: '#282'}} viewBox={viewBox} onClick={this.handleClick}>
+      <svg id='Sheet' className='Sheet' style={{ background: '#282' }} viewBox={viewBox} onClick={this.handleClick}>
         {this.state.nodes.map(this.renderNode)}
       </svg>
     )
