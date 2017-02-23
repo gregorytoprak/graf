b44e4ef66e9603eb3d945e82c77a0be98ed394f2
gdtoprak <gdtoprak@users.noreply.github.com>
2017-02-23 14:24:08 -0500

    Select and delete nodes reduxified

--- src/components/Node.js
+++ src/components/Node.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
 
 class Node extends Component {
+
+  handleClick = (e) => {
+    e.stopPropagation()
+    if (e.shiftKey) {
+      this.props.delete()
+    } else if (!e.shiftKey) {
+      this.props.select()
+    }
+  }
+
+  handleDoubleClick = (e) => {
+    e.stopPropagation()
+  }
+
   render () {
     return (
       <circle className='Node'
         cx={this.props.loc.x} cy={this.props.loc.y}
         r='1' fill='white' strokeWidth='0.05'
         stroke={this.props.selected ? 'dodgerblue' : 'black'}
+        onClick={this.handleClick}
+        onDoubleClick={this.handleDoubleClick}
       />
     )
   }
