d602cd3e67d975ab236a439f684e4011aa02c37d
gdtoprak <gdtoprak@users.noreply.github.com>
2017-03-03 17:06:38 -0500

    Use cookies instead of localStorage

--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import { viewport } from 'verge'
+import Cookies from 'js-cookie'
 import reducer from './reducers'
 import { resizeViewport } from './actions/sheet'
 import App from './components/App'
 import './styles.css'
 
-const it = window.localStorage.getItem('graph')
+const it = Cookies.get('graph')
 const persistedState = it ? JSON.parse(it) : {}
 const store = createStore(reducer, persistedState)
 
 store.subscribe(() => {
   const currentState = store.getState()
-  window.localStorage.setItem('graph', JSON.stringify(currentState))
+  Cookies.set('graph', JSON.stringify(currentState))
 })
 
 const resize = () => { store.dispatch(resizeViewport(viewport())) }
